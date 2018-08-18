d3201a3ee8df9101602e6685b71991602be1b65a
casali1 <frank_casali@hotmail.com>
2018-08-18 16:03:47 -0600

    Refactored code.

--- WebAPI_AngularJs_Operation/Scripts/employeeController.js
+++ WebAPI_AngularJs_Operation/Scripts/employeeController.js
@@ -1,26 +1,21 @@
-﻿
+﻿//The below code will fetch the data from EmployeeService and will pass to the $scope variable
 
 employeeAppModule.controller('EmployeeCtrl', function ($scope, EmployeeService) //injected the Employee Service factory
 {
 	getEmployeeRecords();  //call the function
 
 	function getEmployeeRecords() {
-		EmployeeService.getEmployees() //invoking the getEmployees() method of the EmployeeService factory
+		var employees = EmployeeService.getEmployees(); //invoking the getEmployees() method of the EmployeeService factory
 
-			.success(function (data) { //case when records found successfully
+		employees.success(function (data) { //case when records found successfully
+			console.log(data);  //just for display in the console
+			$scope.employees = data;
+		})
 
-				console.log(data);  //just for display in the console
-
-				$scope.employees = data;
-
-			})
-
-			.error(function (data, status) { //if something goes wrong
-
-				console.error('failure loading the employee record', status, data);
-				$scope.employees = {}; //return blank record if something goes wrong
-
-			});
+		employees.error(function (data, status) { //if something goes wrong
+			console.error('failure loading the employee record', status, data);
+			$scope.employees = {}; //return blank record if something goes wrong
+		});
 	}
 });
 
